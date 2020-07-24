d9e6c121b939ecfd467b053d96d3ec3aab25e1f7
Rob Savaglio <robert.savaglio@ontariotechu.net>
2020-07-24 16:24:38 -0400

    Created a vertex buffer, doesn't draw anything yet

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -3,14 +3,25 @@
 
 #include <iostream>
 
+
+/*
+
+Best Documentation: http://docs.gl
+
+*/
+
 int main(void)
 {
+    /////////////////////////////////////////////////
+    ////////////////// Init OpenGL //////////////////
+    /////////////////////////////////////////////////
+
     GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
+    
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "OpenGL Practice", NULL, NULL);
     if (!window)
@@ -18,29 +29,52 @@ int main(void)
         glfwTerminate();
         return -1;
     }
-
+    
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
+    
     // Init glew, after the context
     if (glewInit() != GLEW_OK)
         std::cout << "GLEW: glewInit() did not work!" << std::endl;
-
-    // Get GL info
+   
+    // Output OpenGL info
     std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
 
+    /////////////////////////////////////////////////
+    ////////////////// Buffer Data //////////////////
+    /////////////////////////////////////////////////
+
+    float triangleVertices[6] = 
+    {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+    
+    unsigned int bufferID;
+    glGenBuffers(1, &bufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, bufferID);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), triangleVertices, GL_STATIC_DRAW);
+    
+    /////////////////////////////////////////////////
+    ////////////////// Main Loop ////////////////////
+    /////////////////////////////////////////////////
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw a triangle in immediate mode
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        //////// Draw Stuff Here ////////
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        
+        
+       
+        
+        /////////////////////////////////
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
